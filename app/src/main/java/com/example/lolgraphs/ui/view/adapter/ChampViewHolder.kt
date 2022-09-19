42a3f5ba224da2016393057f1c4d8c225312402e
CebollaRodriguez <sangabo777@gmail.com>
2022-09-18 20:28:57 -0500

    Revert "5/10"
    This reverts commit c65a668f991577569bedda12971ed59c589b80ab.

--- app/src/main/java/com/example/lolgraphs/ui/view/adapter/ChampViewHolder.kt
+++ app/src/main/java/com/example/lolgraphs/ui/view/adapter/ChampViewHolder.kt
@@ -1,39 +1,22 @@
 package com.example.lolgraphs.ui.view.adapter
 
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
-import com.example.lolgraphs.core.extensions.HomeRecyclerViewItem
-import com.example.lolgraphs.databinding.ItemMiniChampPlaceHolderBinding
-import com.example.lolgraphs.databinding.ItemMiniChampResultBinding
+import com.example.lolgraphs.data.model.ChampionDc
+import com.example.lolgraphs.databinding.ItemResultBinding
+import com.example.lolgraphs.domain.model.ChampModel
 import com.squareup.picasso.Picasso
 
-sealed class ChampViewHolder(binding: ViewBinding):RecyclerView.ViewHolder(binding.root){
+class ChampViewHolder(view: View):RecyclerView.ViewHolder(view) {
 
-    class ChampionListLoading(
-        private val binding: ItemMiniChampPlaceHolderBinding
-    ): ChampViewHolder(binding){
-        fun bind(){}
-    }
+    private val binding = ItemResultBinding.bind(view)
 
-    class ChampionListResult(
-        private val binding: ItemMiniChampResultBinding
-    ): ChampViewHolder(binding){
-        fun bind(champResult: HomeRecyclerViewItem.ChampResult){
-            binding.tvHeroName.text = champResult.champModel.name
-                Picasso.get().load("http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/${champResult.champModel.id}.png").into(binding.ivChampPhoto)
-                itemView.setOnClickListener { champResult.onCLick(champResult.champModel) }
-        }
+    fun render(champModel: ChampModel, onCLick: (ChampModel) -> Unit) {
+
+        binding.tvHeroName.text = champModel.name
+        Picasso.get().load("http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/${champModel.id}.png").into(binding.ivChampPhoto)
+        itemView.setOnClickListener { onCLick(champModel) }
     }
 
 }
-
-    //private val binding = ItemResultBinding.bind(view)
-//
-    //fun render(champModel: ChampModel, onCLick: (ChampModel) -> Unit) {
-//
-    //    binding.tvHeroName.text = champModel.name
-    //    Picasso.get().load("http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/${champModel.id}.png").into(binding.ivChampPhoto)
-    //    itemView.setOnClickListener { onCLick(champModel) }
-    //}
-
