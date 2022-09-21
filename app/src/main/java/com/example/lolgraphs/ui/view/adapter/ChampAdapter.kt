a7228055b701aee2bcdb56d321fe325cc53e3f8e
CebollaRodriguez <sangabo777@gmail.com>
2022-09-20 21:47:39 -0500

    Revert "Revert "Revert "Revert "Revert "5/10"""""
    This reverts commit ffa4389e8270caa4b57100f57b6802a7bb98985e.

--- app/src/main/java/com/example/lolgraphs/ui/view/adapter/ChampAdapter.kt
+++ app/src/main/java/com/example/lolgraphs/ui/view/adapter/ChampAdapter.kt
@@ -5,62 +5,20 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lolgraphs.R
-import com.example.lolgraphs.core.extensions.HomeRecyclerViewItem
-import com.example.lolgraphs.databinding.ItemMiniChampPlaceHolderBinding
-import com.example.lolgraphs.databinding.ItemMiniChampResultBinding
 import com.example.lolgraphs.domain.model.ChampModel
-import java.lang.IllegalArgumentException
 
-class ChampAdapter(
-    //private var champMap: Map<String, ChampModel>,
-    //private var onCLick: (ChampModel) -> Unit
-):RecyclerView.Adapter<ChampViewHolder>() {
-
-    var items = listOf<HomeRecyclerViewItem>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
+class ChampAdapter(private var champMap: Map<String, ChampModel>, private var onCLick: (ChampModel) -> Unit):RecyclerView.Adapter<ChampViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChampViewHolder {
-        return when(viewType){
-            R.layout.item_mini_champ_place_holder -> ChampViewHolder.ChampionListLoading(
-                ItemMiniChampPlaceHolderBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-            )
-            R.layout.item_mini_champ_result -> ChampViewHolder.ChampionListResult(
-                ItemMiniChampResultBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-            )
-            else -> throw IllegalArgumentException("Invalid ViewType Provider")
-        }
-
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ChampViewHolder(layoutInflater.inflate(R.layout.item_result, parent, false))
     }
 
     override fun onBindViewHolder(holder: ChampViewHolder, position: Int) {
-        when(holder){
-            is ChampViewHolder.ChampionListLoading -> holder.bind()
-            is ChampViewHolder.ChampionListResult -> holder.bind(items[position] as HomeRecyclerViewItem.ChampResult)
-        }
-        //val item = champMap.values.toTypedArray()[position]
-        //holder.render(item,onCLick)
+        val item = champMap.values.toTypedArray()[position]
+        holder.render(item,onCLick)
     }
 
-
-    override fun getItemCount(): Int = items.size
-
-    override fun getItemViewType(position: Int): Int {
-        return when(items[position]){
-            is HomeRecyclerViewItem.ChampLoading -> R.layout.item_mini_champ_place_holder
-            is HomeRecyclerViewItem.ChampResult -> R.layout.item_mini_champ_result
-        }
-    }
+    override fun getItemCount(): Int = champMap.keys.size
 }
 
 
